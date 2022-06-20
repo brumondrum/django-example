37b8fb755dba5c366d609b2674c2540b7f4a0d43
Ryan Brum <rbrum2005@gmail.com>
2022-06-20 07:54:21 -0700

    'first'

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# class UserProfileInfo(models.Model):
+
+#     user = models.OneToOneField(User)
+
+#     portfolio_site = models.URLField(blank=True)
+
+#     profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+#     def __str__(self):
+#         return self.user.username
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User)
+
+    # Add any additional attributes you want
+    portfolio_site = models.URLField(blank=True)
+    # pip install pillow to use this!
+    # Optional: pip install pillow --global-option=”build_ext” --global-option=”--disable-jpeg”
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User !
+        return self.user.username
